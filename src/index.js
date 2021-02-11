2a6ae1f950f458ea6ba72794652188230724c85d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-10 22:20:41 -0600

    37:40

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const engine = require('ejs-mate')
+const morgan = require('morgan')
+const path = require('path')
 
 const app = express()
 
 //settings
 app.set('port', process.env.PORT || 4444)
+app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', engine)
 app.set('view engine', 'ejs')
+
+//Middlewares
+app.use(morgan('dev'))
+
+//Routes
+app.use('', require('./routes/index.routes'))
 
 //starting the server
 app.listen(app.get('port'), () => {
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {})
+router.get('/', (req, res, next) => {
+	res.render('index')
+})
 
-router.get('/signup', (req, res, next) => {})
+router.get('/signup', (req, res, next) => {
+	res.render('signup')
+})
 
-router.post('/signup', (req, res, next) => {})
+router.post('/signup', (req, res, next) => {
+	res.send('enviado')
+})
 
 router.get('/login', (req, res, next) => {})
 
